eb0fb42970a993d683b18e1f76a5fc95ee3e06b5
antonystar-dev <antonystar.contatos@gmail.com>
2024-03-30 05:53:35 +0000

    pagina inicio - 20%

--- index.php
+++ index.php
@@ -2,13 +2,18 @@
 <html lang="pt-BR">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Lorikeet - moda infantil</title>
-    <link href="#" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lorikeet - Moda infantil</title>
+    <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+
 </head>
 
 <body>
+
 
   <div>
     menu
@@ -23,12 +28,28 @@
   </div>
     <div>
 
-     produtos
+    
+     <?php
+     include("view/index.html");
+     ?>
+</div>
+</div>
     
   </div>
   <div>
+    <br><br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    
     rodape
   </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
